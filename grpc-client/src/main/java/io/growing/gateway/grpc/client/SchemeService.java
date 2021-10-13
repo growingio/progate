9d3e15841dc37591463b26fccebbdb331b70b07e
zhuhongbin <zhb1316783262@163.com>
2021-10-13 14:24:40 +0800

    只保留默认构造函数，添加getset 设置classloader

--- grpc-client/src/main/java/io/growing/gateway/grpc/client/SchemeService.java
+++ grpc-client/src/main/java/io/growing/gateway/grpc/client/SchemeService.java
@@ -13,6 +13,7 @@ public class SchemeService extends SchemeServiceGrpc.SchemeServiceImplBase {
 
     private ClassLoader[] classLoaders;
 
+
     public static SchemeService newInstance() {
         return new SchemeService();
     }
@@ -21,18 +22,14 @@ public class SchemeService extends SchemeServiceGrpc.SchemeServiceImplBase {
     private final ClasspathOpenApiSchemaScanner openApiSchemaScanner = new ClasspathOpenApiSchemaScanner();
 
     public SchemeService() {
-        this.classLoaders = new ClassLoader[]{this.getClass().getClassLoader()};
-    }
-
-    public SchemeService(ClassLoader[] classLoaders) {
-        this.classLoaders = classLoaders;
+        this.setClassLoaders(new ClassLoader[]{this.getClass().getClassLoader()});
     }
 
     @Override
     public void getScheme(Empty request, StreamObserver<SchemeDto> responseObserver) {
         try {
-            final List<FileDescriptorDto> graphqlFiles = graphqlSchemaScanner.scan(this.classLoaders,"graphql");
-            final List<FileDescriptorDto> restfulFiles = openApiSchemaScanner.scan(this.classLoaders,"restful");
+            final List<FileDescriptorDto> graphqlFiles = graphqlSchemaScanner.scan(this.classLoaders, "graphql");
+            final List<FileDescriptorDto> restfulFiles = openApiSchemaScanner.scan(this.classLoaders, "restful");
             final SchemeDto scheme = SchemeDto.newBuilder().addAllRestfulDefinitions(restfulFiles).addAllGraphqlDefinitions(graphqlFiles).build();
             responseObserver.onNext(scheme);
         } catch (IOException e) {
@@ -41,4 +38,11 @@ public class SchemeService extends SchemeServiceGrpc.SchemeServiceImplBase {
         responseObserver.onCompleted();
     }
 
+    public ClassLoader[] getClassLoaders() {
+        return classLoaders;
+    }
+
+    public void setClassLoaders(ClassLoader[] classLoaders) {
+        this.classLoaders = classLoaders;
+    }
 }
